499a93cdb94bcc666e7241751da60b26aaab0024
xksyl <egorka2004212@gmail.com>
2024-11-30 15:52:13 +0500

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range (1,6):
+        await asyncio.sleep(1/power)
+        print(f'Силач {name} поднял {i} шар')
+        print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    strongman = [start_strongman('Pasha', 3),
+                 start_strongman('Denis', 4),
+                 start_strongman('Apollon', 5)]
+    await asyncio.gather(*strongman)
+
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
+
+
+
+
